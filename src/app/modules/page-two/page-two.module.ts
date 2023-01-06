9de2e2578e0447f58f87d41180834dbdc239bf6b
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-01-06 21:03:16 +0330

    chore(globalRouting)!: implement routing system

--- src/app/modules/page-two/page-two.module.ts
+++ src/app/modules/page-two/page-two.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageTwoComponent } from './page-two.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PageTwoComponent,
+  },
+];
 
 @NgModule({
   declarations: [PageTwoComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [PageTwoComponent],
 })
 export class PageTwoModule {}
