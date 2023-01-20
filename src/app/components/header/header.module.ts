1e15772e9366b6a1faebb7179ceb835b3b53c9d3
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-01-20 22:20:06 +0330

    feat(header): implement the header component

---
+++ src/app/components/header/header.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule],
+})
+export class HeaderModule {}
