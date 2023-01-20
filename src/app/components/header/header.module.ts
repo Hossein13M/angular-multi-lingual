16844749fffc3e6190a041c8472db5b1d5a76339
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-01-20 22:53:03 +0330

    style(global): enhance page layout

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { LanguageSelectorModule } from '../language-selector/language-selector.module';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, LanguageSelectorModule],
 })
 export class HeaderModule {}
